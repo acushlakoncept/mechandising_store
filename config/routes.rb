2dbf91b9e55a3ea314bd69492c7e937445d275e7
Uduak Essien <acushla4real@gmail.com>
2022-03-03 12:29:23 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: %i[index update]
+      post '/items/check_price', to: 'items#check_price'
+      post '/items/apply_discount', to: 'items#apply_discount'
+    end
+  end
 end
